8d9ca85e1043f26cb07891cfaeb81c444f5f969d
sanketkumar <sanket.kr7922@gmail.com>
2019-01-11 08:28:15 +0530

    Add Resource file
    Build Basic UI with Stack View

--- Tinder/ViewController.swift
+++ Tinder/ViewController.swift
@@ -12,7 +12,32 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let subViews = [UIColor.gray,UIColor.darkGray,UIColor.black].map { (color) -> UIView in
+            let v = UIView()
+            v.backgroundColor = color
+            return v
+        }
+        
+        let topStackView = UIStackView(arrangedSubviews: subViews)
+        topStackView.distribution = .fillEqually
+        topStackView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        
+        let blueView = UIView()
+        blueView.backgroundColor = .blue
+        
+        let yellowView = UIView()
+        yellowView.backgroundColor = .yellow
+        yellowView.heightAnchor.constraint(equalToConstant: 120).isActive = true
+        
+        let stackView = UIStackView(arrangedSubviews:[topStackView,blueView,yellowView])
+        
+        stackView.axis = .vertical
+        self.view.addSubview(stackView)
+        
+        // Fill the entire super view
+        stackView.fillSuperview()
+        
     }
 
 
